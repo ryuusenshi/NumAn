634393aa47491eac51bdad466c379ac7bf8515d5
David Filipovic <david.filipovic@gmail.com>
2012-05-02 12:12:22 -0400

    improved the geometry of the key, changed to only direct neighbors

--- GRADIENTexample.m
+++ GRADIENTexample.m
@@ -4,13 +4,13 @@ clc;
 %   seting up the grid dimensions, voltages of the container and the key
 c_voltage = 0;
 key_voltage = 5;
-hor_dim = 51;
-ver_dim = 51;
+hor_dim = 160;
+ver_dim = 160;
 
 %%
 %DEFININING THE CONTAINER
 %   assumed rectangular
-xv=[1 10 10 1 1];
+xv=[1 9 9 1 1];
 yv=[1 1 6 6 1];
 hold on;
 plot(xv,yv);
@@ -19,13 +19,22 @@ axis equal;
 %%
 %DEFINING THE KEY
 %   assumed a polygon
-xv2=[5 7 7 5 5 4 3.75 4 5 5];
-yv2=[4 4 5 5 4.75 4.75 4.5 4.25 4.25 4];
-plot(xv,yv);
-plot(xv2,yv2);
+%xv2=[5 7 7 5 5 4 3.75 4 5 5];
+%yv2=[4 4 5 5 4.75 4.75 4.5 4.25 4.25 4];
+xv2=[2 2.4 6 6 6.4 7 7.4 7.4 7 6.4 6 5.7 5.6 5.3 5.2 4.8 4.6 4.2 4 3.75 3.5 3.25 3 2.4 2];
+yv2=[4 4.3 4.3 4.8 4.95 4.95 4.8 3.5 3.35 3.35 3.7 3.7 3.9 3.9 3.7 3.7 3.8 3.8 3.7 3.95 3.7 3.85 3.7 3.7 4];
+%hole in the key
+t = linspace(0,2*pi,25);
+h=6.9;
+k=4.5;
+r=0.2;
+xhole = r*cos(t)+h;
+yhole = r*sin(t)+k; 
 
 
-%should correct this to work even if the container is not rectangulat
+
+
+%should correct this to work even if the container is not rectangular
 %   define the grid and then use only grid points that are inside the
 %   container!
 
@@ -47,8 +56,14 @@ yh=ones(ver_dim^2,1);
 for i=0:ver_dim-1
     yh(i*ver_dim+1:(i+1)*ver_dim) = y(1:ver_dim);
 end
-in = inpolygon(xh,yh,xv2,yv2);
+inkey = inpolygon(xh,yh,xv2,yv2);
+inhole = inpolygon(xh,yh,xhole,yhole);
+in=xor(inkey,inhole);
 plot(xh(in),yh(in),'.r',xh(~in),yh(~in),'.b');
+
+plot(xv,yv,'k','LineWidth',2);
+plot(xv2,yv2,'k','LineWidth',2);
+plot(xhole,yhole,'k','LineWidth',2);
 
 
 
@@ -87,8 +102,10 @@ pause;
 %while(1)
 for loopn = 1:50
 for i=1:ver_dim
-    mask = ones(3,3);
-    mask(2,2) = 0;
+    mask = [0 1 0;
+            1 0 1;
+            0 1 0];
+    
     for j=1:hor_dim
         if (fixed(i,j)==0)
             if (i+1>ver_dim)
